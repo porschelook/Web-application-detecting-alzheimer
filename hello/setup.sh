5e4e1448690085bda1f7678032d7b68c8cafee1a
u <potschelook@gmail.com>
2020-05-05 16:49:45 +0700

    eiei
    eiei

---
+++ hello/setup.sh
@@ -0,0 +1,6 @@
+pip install django
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate --run-syncdb
+python manage.py runserver 0.0.0.0:80
